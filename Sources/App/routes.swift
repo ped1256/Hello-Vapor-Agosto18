ce8df50efcbdcc9d874505e69efbdb305fa06e64
Pedro Emanuel <pedro.azevedo@peixeurbano.com>
2018-08-15 17:48:54 -0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -12,4 +13,48 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    
+    router.get("hello", String.parameter) { req -> String in
+        guard let name = req.parameters.values.first?.value else { return "null" }
+
+        return "Hello \(name) !"
+    }
+
+//    router.post("info") { req -> InfoResponse in
+//        let data = try req.content.decode(InfoData.self)
+//
+//    }
+    
+    router.get("count",Int.parameter) { req -> NumberJSON in
+        let count = try req.parameters.next(Int.self)
+        return NumberJSON(count: count)
+    }
+    
+    router.get("date") { req in
+        return String("\(Date())")
+    }
+    
+    router.post("user-info"){ req -> UserInfoJSON in
+        let data = try req.content.syncDecode(UserInfoJSON.self)
+        return UserInfoJSON(name: data.name, age: data.age)
+    }
 }
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
+}
+struct NumberJSON: Content {
+    let count: Int
+}
+struct UserInfoJSON: Content {
+    let name: String
+    let age: Int
+}
+
+
+
